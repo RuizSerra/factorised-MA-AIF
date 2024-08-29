50aee60386b0d956d8ca1292063a2ed4de381886
pfsweeney <84429424+pfsweeney@users.noreply.github.com>
2024-08-29 22:24:40 +1000

    New games

--- games.py
+++ games.py
@@ -12,6 +12,7 @@ import torch
 # Add a small CONSTANT to avoid log(0) issues
 CONSTANT = 1
 NEG_CONSTANT = -5
+EPSILON = 1e-09
 
 # ==============================================================================
 # 2 Player, 2 action games
@@ -46,6 +47,39 @@ matching_pennies_2player = torch.tensor(    # Matching pennies (row player payof
      [-1.0,  1.0]]      # T H               #       G_c = -G_r
 )                                           #       e.g., to load, game_matrix=[(-1)**i * matching_pennies_2player for i in range(num_players)][i]
 
+
+
+#Fisherman's Dilemma (Bowles)
+alpha = 1.0  # Temptation surplus: Set this to any desired value
+u = 2.0  # Fallback position / reservation option / BATNA: Set this to any desired value
+fishers_dilemma_2player = torch.tensor([
+    [1.0, 0.0],         # Jay fishes 6 hours (Row 1)
+    [1.0 + alpha, u] + EPSILON   # Jay fishes 8 hours (Row 2)
+])
+
+
+# Firm Survival Game (Bowles)
+p1 = 0.5  # Probability firm succeeds if the employer invests and the worker does not work:  Set this to any desired value, where 1 > p1 > p2 > 0
+p2 = 0.2  # # Probability firm succeeds if the employer does not invest and the worker does work: Set this to any desired value, where p1 > p2 > 0
+firm_survival_game_2player = torch.tensor([
+    [1.0, p2],         # Worker works (Row 1): Employer invests, Employer does not invest
+    [p1, 0.0]      + EPSILON  # Worker does not work (Row 2): Employer invests, Employer does not invest
+])
+
+
+# Invisible Hand Game (Bowles)
+invisible_hand_game_2player_row = torch.tensor([
+    [2, 4],  # Player 1 chooses Corn
+    [5, 3]   # Player 1 chooses Tomatoes
+])
+
+# Payoff matrix for Player 2 (Column player)
+invisible_hand_game_2player_column = torch.tensor([
+    [4, 3],  # Player 2 chooses Corn
+    [5, 2]   # Player 2 chooses Tomatoes
+])
+
+
 # ==============================================================================
 # 2 Player, 3 action games
 # ==============================================================================
@@ -67,6 +101,22 @@ test_game_2player = torch.tensor(
      [1., 1., 1.],
      [10., 1., 1.]]
 )
+
+
+# ==============================================================================
+# 2 Player, 5 action games
+# ==============================================================================
+
+division_game_2player = torch.tensor([
+    [[100, 0], [80, 20], [60, 40], [40, 60], [20, 80], [0, 100]],  # Player 1 claims 100
+    [[80, 20], [60, 40], [40, 60], [20, 80], [0, 100], [100, 0]],  # Player 1 claims 80
+    [[60, 40], [40, 60], [20, 80], [0, 100], [100, 0], [80, 20]],  # Player 1 claims 60
+    [[40, 60], [20, 80], [0, 100], [100, 0], [80, 20], [60, 40]],  # Player 1 claims 40
+    [[20, 80], [0, 100], [100, 0], [80, 20], [60, 40], [40, 60]],  # Player 1 claims 20
+    [[0, 100], [100, 0], [80, 20], [60, 40], [40, 60], [20, 80]],  # Player 1 claims 0
+])
+
+
 
 # ==============================================================================
 # 3 Player, 2 action games
