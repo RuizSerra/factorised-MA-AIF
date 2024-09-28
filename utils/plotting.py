54fef3642cf43fbdcf6f43b7211d5cababf8993c
Jaime <jrs271828@gmail.com>
2024-09-28 12:28:12 +1000

    margin

--- utils/plotting.py
+++ utils/plotting.py
@@ -13,6 +13,7 @@ DPI = 72
 SHOW_LEGEND = False
 ONLY_LEFT_Y_LABEL = False
 TIGHT_LAYOUT = False
+MARGIN = 0.05  # Percentage margin for ylim
 
 # Define consistent color settings
 VFE_COLOR = '#ff00ff'
@@ -261,6 +262,10 @@ def plot_vfe(
     
     ax.set_title(f'VFE Agent {chr(105+i)}', fontsize=label_font_size)
     ax.set_xlabel('Time step (t)', fontsize=label_font_size)
+    # ylim range based on all agents (not just the current agent i)
+    ax.set_ylim(
+        (1-MARGIN)*vfe_history[t_min:t_max].sum(axis=2).min(), 
+        (1+MARGIN)*vfe_history[t_min:t_max].sum(axis=2).max())
     if not ONLY_LEFT_Y_LABEL or (ONLY_LEFT_Y_LABEL and i == 0):
         ax.set_ylabel('VFE', color='black', fontsize=label_font_size)
         ax2.set_ylabel('Nats', fontsize=label_font_size)
@@ -354,6 +359,10 @@ def plot_efe(
     
     ax.set_title(f'EFE Agent {chr(105+i)}', fontsize=label_font_size)
     ax.set_xlabel('Time step (t)', fontsize=label_font_size)
+    # ylim range based on all agents (not just the current agent i)
+    ax.set_ylim(
+        (1-MARGIN)*efe_history[t_min:t_max].min(), 
+        (1+MARGIN)*efe_history[t_min:t_max].max())
     if not ONLY_LEFT_Y_LABEL or (ONLY_LEFT_Y_LABEL and i == 0):
         ax.set_ylabel('G[u]', color='black', fontsize=label_font_size)
     if SHOW_LEGEND:
@@ -371,6 +380,10 @@ def plot_expected_efe(
 
     t_max = len(efe_history) if t_max is None else t_max
     x_range = range(t_min, t_max)
+
+    # Only plot EFE terms if they are non-zero (i.e. if actual data was provided)
+    # (as a way to stub out the plotting of EFE terms)
+    plot_EFE_terms = (not np.all(risk_history == 0))
 
     # Get EFE, Pragmatic Value, Salience, Risk, and Ambiguity over all actions
     # Calculate the dot product with q_u_history for each agent (i.e. expected value undre action dist)
@@ -403,42 +416,49 @@ def plot_expected_efe(
         summed_efe, 
         label='E[G]', color=EFE_COLOR, linewidth=1.5)
     
-    # Create a secondary y-axis for Risk, Ambiguity, Salience, Pragmatic Value, and Novelty)
-    ax2 = ax.twinx()
-    ax2.plot(
-        x_range, 
-        summed_risk, 
-        label='Risk', color=RISK_COLOR, linestyle=':', linewidth=1.2)
-    ax2.plot(
-        x_range, 
-        summed_ambiguity, 
-        label='Ambiguity', color=AMBIGUITY_COLOR, linestyle=':', linewidth=1.2)
-    ax2.plot(
-        x_range, 
-        summed_salience, 
-        label='Salience', color=SALIENCE_COLOR, linestyle=':', linewidth=1.2)
-    ax2.plot(
-        x_range, 
-        summed_pragmatic_value, 
-        label='Pragmatic Value', color=PRAGMATIC_COLOR, linestyle=':', linewidth=1.2)
-    
-    # Plot Novelty if provided
-    if summed_novelty is not None:
+    if plot_EFE_terms:
+        # Create a secondary y-axis for Risk, Ambiguity, Salience, Pragmatic Value, and Novelty)
+        ax2 = ax.twinx()
         ax2.plot(
             x_range, 
-            summed_novelty, 
-            label='Novelty', color=NOVELTY_COLOR, linestyle=':', linewidth=1.2)
+            summed_risk, 
+            label='Risk', color=RISK_COLOR, linestyle=':', linewidth=1.2)
+        ax2.plot(
+            x_range, 
+            summed_ambiguity, 
+            label='Ambiguity', color=AMBIGUITY_COLOR, linestyle=':', linewidth=1.2)
+        ax2.plot(
+            x_range, 
+            summed_salience, 
+            label='Salience', color=SALIENCE_COLOR, linestyle=':', linewidth=1.2)
+        ax2.plot(
+            x_range, 
+            summed_pragmatic_value, 
+            label='Pragmatic Value', color=PRAGMATIC_COLOR, linestyle=':', linewidth=1.2)
+        
+        # Plot Novelty if provided
+        if summed_novelty is not None:
+            ax2.plot(
+                x_range, 
+                summed_novelty, 
+                label='Novelty', color=NOVELTY_COLOR, linestyle=':', linewidth=1.2)
     
     # Set labels and title
     ax.set_title(f'Expected EFE Agent {chr(105+i)}', fontsize=label_font_size)
     ax.set_xlabel('Time step (t)', fontsize=label_font_size)
+    # ylim range based on all agents (not just the current agent i)
+    ax.set_ylim(
+        (1-MARGIN)*expected_efe[t_min:t_max].min(), 
+        (1+MARGIN)*expected_efe[t_min:t_max].max())
     if not ONLY_LEFT_Y_LABEL or (ONLY_LEFT_Y_LABEL and i == 0):
         ax.set_ylabel('E[G]', color='black', fontsize=label_font_size)
-        ax2.set_ylabel('Nats', fontsize=label_font_size)
+        if plot_EFE_terms:
+            ax2.set_ylabel('Nats', fontsize=label_font_size)
     if SHOW_LEGEND:
         ax.legend(loc='upper left')
-        ax2.legend(loc='upper right')
-        # ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3)
+        if plot_EFE_terms:
+            ax2.legend(loc='upper right')
+            # ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3)
 
 
 def plot_efe_risk_ambiguity(expected_efe, weighted_risk, weighted_ambiguity, ax, i):
@@ -712,6 +732,10 @@ def plot_precision(
     
     ax.set_title(f'Precision for Agent {chr(105+i)}', fontsize=label_font_size)
     ax.set_xlabel('Time step (t)', fontsize=label_font_size)
+    # ylim range based on all agents (not just the current agent i)
+    ax.set_ylim(
+        (1-MARGIN)*gamma_history[t_min:t_max].min(), 
+        (1+MARGIN)*gamma_history[t_min:t_max].max())
     if not ONLY_LEFT_Y_LABEL or (ONLY_LEFT_Y_LABEL and i == 0):
         ax.set_ylabel('Precision', fontsize=label_font_size)
 
